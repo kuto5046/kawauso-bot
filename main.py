a617072e2b1292518951d9f3848c557c165f1eca
kyohei <h27.kubb@gmail.com>
2020-07-29 20:32:12 +0900

    [add] import linebot

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, CarouselTemplate, CarouselColumn
+from linebot.models import (
+    MessageEvent, TextMessage, QuickReplyButton, MessageAction, QuickReply,
+    TextSendMessage, LocationMessage, TemplateSendMessage, CarouselTemplate,
+    CarouselColumn, URITemplateAction)
 import os
 from news import News
 from weather import Weather
 import random
+
+
 
 app = Flask(__name__)
 
